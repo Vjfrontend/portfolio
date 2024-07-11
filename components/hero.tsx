656544a68a5208019e0a0e2e814dad1de9a3c7b8
victor <victorezeabikwa@gmail.com>
2024-07-11 15:24:38 +0100

    little fix

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 
 import Link from "next/link";
 
-import bg from "@/public/images/Hexagon (2).svg";
-import pc from"@/public/images/author-01.jpg";
+import bg from "@/public/images/Meteor 1.svg";
+import pc from "@/public/images/testimonial-01.jpg";
+
+// Skip existing imports
 
 export default function Hero() {
   return (
-    <section className="relative bg-white">
-      <div className="absolute mt-20">
-        <div className="animate-color-change">
+    <section className="relative bg-[#FAFAFA]">
+      <div className="absolute ">
+        <div className="animate-color-change mt-20">
           <Image src={bg} alt="Picture of the author" />
         </div>
       </div>
@@ -22,47 +24,39 @@ export default function Hero() {
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           {/* Hero content */}
           <div className="relative max-w-xl mx-auto md:max-w-none text-center md:text-left flex flex-col md:flex-row">
-            <div className="mt-12 ">
-            {/* <div className="flex justify-center items-center  ">
-
-            <Image src={pc} className="rounded- w-60" alt="Picture of the author" />
-            </div> */}
+            <div className="-mt-8x ">
+              <div className="flex justify-center items-center  ">
+                <Image
+                  src={pc}
+                  className="rounded-full w-60"
+                  alt="Picture of the author"
+                />
+              </div>
               {/* Copy */}
               <div className="flex justify-center items-center">
-                <h1
-                  className="   text-3xl font-gaMaamli mb-36 text-black  "
-                  data-aos=""
-                  data-aos-delay="100"
-                >
-                  I <span className="text-blue-500  text-4xl">'</span>m{" "}
+                <h1 className="text-3xl md:text-4xl lg:text-5xl font-gaMaamli lg:mb-4 md:mb-6  text-black lg:pb-28">
+                  I{" "}
+                  <span className="text-blue-500 text-4xl md:text-5xl lg:text-6xl">
+                    '
+                  </span>
+                  m{" "}
                 </h1>
-                <h1
-                  className="text-9xl mb-6 relative inline-flex justify-center font-gaMaamli text-black"
-                  data-aos-delay="100"
-                >
+                <h1 className="text-4xl md:text-6xl lg:text-8xl mb-2 md:mb-4 mt-16 lg:mt-0 relative inline-flex justify-center font-gaMaamli text-black">
                   Victor Ezeabikwa
                 </h1>
               </div>
 
-              <p
-                className="text-xl text-gray-500 mb-10 flex max-w-[80%] mx-auto text-center "
-                data-aos=""
-                data-aos-delay="200"
-              >
+              <p className="text-sm md:text-xl lg:text-xl text-black mb-4 lg:-mt-10 md:mb-8 lg:mb-12 flex lg:max-w-[80%] mx-auto font-aboreto text-center">
                 Are you looking to build an innovative product, feature, or
                 website? Look no further! With my extensive experience and
                 proven track record, I can help turn your ideas into tangible,
                 successful projects.
               </p>
               {/* Buttons */}
-              <div
-                className="flex justify-center mx-auto "
-                data-aos="f"
-                data-aos-delay="300"
-              >
-                <div className="">
+              <div className="flex  justify-center mx-auto ">
+                <div className="mb-4 md:mb-0 md:mr-4">
                   <Link
-                    className="btn text-white mr-8 bg-blue-500 hover:bg-blue-600   p-4 shadow-md rounded-md"
+                    className="btn text-white bg-blue-500 hover:bg-blue-600 px-3 py-4 md:p-4 lg:p-5 shadow-md rounded-md mr-6"
                     href="#"
                   >
                     Checkout my work
@@ -70,12 +64,12 @@ export default function Hero() {
                 </div>
                 <div>
                   <Link
-                    className="btn text-white bg-black hover:bg-blue-100 hover:text-blue-600 w-full shadow-md rounded-md "
+                    className="btn text-white bg-black hover:bg-blue-100 hover:text-blue-600 px-3 md:p-4 lg:px-10 shadow-md rounded-md"
                     href="#"
                   >
                     Resume
                     <svg
-                    className="pl-2"
+                      className="pl-2"
                       xmlns="http://www.w3.org/2000/svg"
                       width="32"
                       height="32"
